00b5d163bfb3dc69308f1c91f9bfb8ec5acd5da0
Hanul Eum <ceum@uci.edu>
2026-02-04 00:03:25 -0800

    adding changes to launch.py

--- launch.py
+++ launch.py
@@ -5,6 +5,7 @@ from utils.server_registration import get_cache_server
 from utils.config import Config
 from crawler import Crawler
 
+from scraper import write_statistics, unique_urls, unique_subdomains, word_count
 
 def main(config_file, restart):
     cparser = ConfigParser()
@@ -13,6 +14,7 @@ def main(config_file, restart):
     config.cache_server = get_cache_server(config, restart)
     crawler = Crawler(config, restart)
     crawler.start()
+    write_statistics()
 
 
 if __name__ == "__main__":
